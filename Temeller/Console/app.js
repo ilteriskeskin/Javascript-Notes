d5c8fe3d829989c361c89dac97f84b1bd65741b5
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-05-21 22:32:53 +0300

    Temel konsol komutlari denendi

---
+++ Temeller/Console/app.js
@@ -0,0 +1,12 @@
+document.getElementById('h3').style.color = 'green';
+
+console.log('Merhaba Javascript');
+console.log('123');
+console.log(true);
+
+var a = 3;
+console.log(a);
+console.log(['AHMET', 'MEHMET']);
+console.log(typeof a);
+console.warn('Bu bir uyarıdır');
+console.error('Bu bir hatadır');
