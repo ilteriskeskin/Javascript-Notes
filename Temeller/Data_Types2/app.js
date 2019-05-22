f18d6b83050f5279d4aa3d81b1212bb28e096bb2
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-05-23 01:24:35 +0300

    Veri tiplerine devam edildi

---
+++ Temeller/Data_Types2/app.js
@@ -0,0 +1,29 @@
+// Var
+
+var a;
+a = 20;
+console.log(a);
+
+var name = "Ali İlteriş";
+console.log(name);
+var name = "Ali";
+console.log(name);
+
+// Let
+
+let name2 = "Ali İlteriş";
+console.log(name2);
+
+// let name2 = "Ali"; Hata verir
+
+// Const
+
+const name3 = "Ali İlteriş";
+console.log(name3);
+
+// name = "Ali"; Hata verir 
+
+const b = [1, 2, 3, 4];
+console.log(b);
+b.push(5);
+console.log(b);
