19f29bc31bbadaae56d44d0994859270deb39896
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-05-25 02:58:53 +0300

    Operatorler ve math objesi ile ilgili ornek eklendi

--- Temeller/Operators/app.js
+++ Temeller/Operators/app.js
@@ -2,4 +2,47 @@ let value;
 const val1 = 10;
 const val2 = 4;
 
+
+// Aritmetik Operatörler
+
+value = val1 + val2;
 console.log(value);
+value = val1 - val2;
+console.log(value);
+value = val1 * val2;
+console.log(value);
+value = val1 / val2;
+console.log(value);
+value = val1 % val2;
+console.log(value);
+
+// Math Objesi
+
+value = Math.PI;
+console.log(value);
+value = Math.E;
+console.log(value);
+
+value = Math.round(value);
+console.log(value);
+value = Math.ceil(value);
+console.log(value);
+value = Math.floor(value);
+console.log(value);
+
+value = Math.sqrt(16);
+console.log(value);
+value = Math.abs(-20);
+console.log(value);
+value = Math.pow(5,3);
+console.log(value);
+
+value = Math.max(10, -3, 123, 32);
+console.log(value);
+value = Math.min(10, -3, 123, 32);
+console.log(value);
+
+value = Math.random();
+console.log(value);
+value = Math.random() * 20; // 0-19 arası sayılar
+value = Math.floor(Math.random() * 20); // 0-19 arası int değerler
